1c73a6484d504e339531db477df26425a0c45706
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-11-08 14:29:14 -0700

    Create circle.h

---
+++ circle.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int circle_sort_inner(int *start, int *end) {
+  int *p, *q, t, swapped;
+
+  if (start == end) 
+    return 0;
+
+  // "||" on next line is for the center element of odd-lengthed array.
+  for (swapped = 0, p = start, q = end; p<q || (p==q && ++q); p++, q--)
+    if (*p > *q)
+      t = *p, *p = *q, *q = t, swapped = 1;
+      
+  // q == p-1 at this point
+  return swapped | circle_sort_inner(start, q) | circle_sort_inner(p, end);
+}
+
+// Helper function to show arrays before each call.
+void circle_sort(int *x, int n) 
+{
+  do 
+  {
+    for (int i = 0; i < n; i++) 
+      printf("%d ", x[i]);
+    putchar('\n');
+  } while (circle_sort_inner(x, x + (n - 1)));
+}
