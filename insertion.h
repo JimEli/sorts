6c88f57f1286eb4d5bc05194f5c99deec87b7aae
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-11-08 16:06:34 -0700

    Update insertion.h

--- insertion.h
+++ insertion.h
@@ -10,3 +10,40 @@ void insertionSort(T data[], const std::size_t n)
     data[j] = tmp;
   }
 }
+
+/*************************************************************************
+ * Implements the insertion sort algorithm.
+ *************************************************************************/
+template<class T>
+void insertionSort(T* begin, T* end)
+{
+  for (T* i = begin; i != end; i++)
+  {
+    T tmp = *i, *j;
+
+    for (j = i; j >= begin && tmp < *(j - 1); j--)
+      *j = *(j - 1);
+    *j = tmp;
+  }
+}
+
+/*
+// Binary insertion sort O(n^2) time O(1) space, binary search to insert instead of swapping
+template <typename T>
+void insertion_sort(T* begin, T* end, T* cur) 
+{
+  for (; cur < end; ++cur)
+  {
+    T pivot = std::move(*cur);
+
+    // upper_bound is binary search for correct insertion position
+    const T pos = std::upper_bound(begin, cur, pivot);
+    for (T* ins = cur; ins != pos; --ins)
+      *ins = std::move(*(ins - 1));
+    *pos = std::move(pivot);
+  }
+}
+
+template <typename T>
+void insertionSort(T* begin, T* end) { insertion_sort(begin, end, begin); }
+*/
