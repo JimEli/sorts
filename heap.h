b4206caae2b98135a53e2483339ec86233ced3bb
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-11-08 16:11:43 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -33,3 +33,42 @@ void heapSort(T data[], const int n)
     moveDown(data, 0, i - 1);  // restore the heap property;
   }
 }
+
+/*************************************************************************
+ * Implements an alternative heap sort algorithm.
+ *************************************************************************/
+template<class T>
+void moveDown(T data[], int first, int last)
+{
+  int largest = 2 * first + 1;
+
+  while (largest <= last)
+  {
+    if (largest < last && data[largest] < data[largest + 1])
+      largest++;
+
+    if (data[first] < data[largest])
+    {
+      std::swap(data[first], data[largest]);
+      first = largest;
+      largest = 2 * first + 1;
+    }
+    else
+      largest = last + 1;
+  }
+}
+
+template<class T>
+void myHeapSort(T* begin, T* end)
+{
+  int n = std::distance(begin, end);
+
+  for (int i = n / 2 - 1; i >= 0; --i)
+    moveDown(begin, i, n - 1);
+
+  for (int i = n - 1; i >= 1; --i)
+  {
+    std::swap(*begin, *(begin + i));
+    moveDown(begin, 0, i - 1);
+  }
+}
