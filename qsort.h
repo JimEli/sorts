a64ecd19f0b34bed8d7c8604a3e5c1922ccacb0d
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-11-08 16:26:50 -0700

    Update qsort.h

--- qsort.h
+++ qsort.h
@@ -6,3 +6,12 @@ T cmp(const void *a, const void *b) { return (*(T*)a - *(T*)b); }
 
 template <typename T>
 void qSort(T* begin, T* end) { qsort(begin, std::distance(begin, end), sizeof(T), cmp); }
+
+/*************************************************************************
+ * C++ STL <algorithm> library qsort function.
+*************************************************************************/
+template<typename T>
+T compare(const void *a, const void *b) { return (*(T*)a - *(T*)b); }
+
+template<typename T>
+void stlQSort(T* begin, T* end) { std::qsort(begin, std::distance(begin, end), sizeof(T), compare); }
