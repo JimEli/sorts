21d91c5b506e18622a21473a9d0acc1e8beab636
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-11-08 16:27:34 -0700

    Create stlStableSort.h

---
+++ stlStableSort.h
@@ -0,0 +1,5 @@
+/*************************************************************************
+ * C++ STL <algorithm> library stable sort (merge) function.
+*************************************************************************/
+template<typename T>
+void stableSort(T* begin, T* end) { std::stable_sort(begin, end); }
